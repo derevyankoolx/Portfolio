2738537a2783fdb05f8421294e2e939bc057c618
devorx <derevyankoolx@gmail.com>
2022-09-12 18:51:07 +0300

    ended desktop version

---
+++ js/main.js
@@ -0,0 +1,15 @@
+$(function (){
+    $('.portfolio__slider').slick({
+        // fade: true,
+        arrows: true
+    });
+});
+
+const anchors = document.getElementByClassName('.link');
+
+for (let anchor of anchors) {
+    
+}   amchoe.addEventListener("click", function(event) {
+    
+})
+  
